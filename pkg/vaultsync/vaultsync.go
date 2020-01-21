7495140db45cd557bc262da1389bcb44559e4cd8
thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>
2020-01-20 23:41:21 -0800

    remove logic from main
    Signed-off-by: thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>

---
+++ pkg/vaultsync/vaultsync.go
@@ -0,0 +1,39 @@
+package vaultsync
+
+import (
+	"log"
+
+	"github.com/thatInfrastructureGuy/VaultSync/v0.0.1/pkg/consumer"
+	"github.com/thatInfrastructureGuy/VaultSync/v0.0.1/pkg/vault"
+)
+
+func Synchronize() {
+	// Select the destination
+	destination, err := consumer.SelectConsumer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get lastUpdated date timestamp from consumer
+	destinationlastUpdated, err := destination.GetLastUpdatedDate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Select the source
+	source, err := vault.SelectProvider(destinationlastUpdated)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Poll secrets from vault which were updated since lastUpdated value
+	secretList, err := source.GetSecrets()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Update kuberenetes secrets
+	err = destination.PostSecrets(secretList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
