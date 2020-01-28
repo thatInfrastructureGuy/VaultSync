646c29f3800c105d24573b9761dc7dfa2bdfc533
thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>
2020-01-27 21:39:49 -0800

    allow for empty strings
    Signed-off-by: thatInfrastructureGuy <thatInfrastructureGuy@gmail.com>

--- pkg/common/data/env.go
+++ pkg/common/data/env.go
@@ -20,51 +20,46 @@ type Env struct {
 }
 
 func (e *Env) Getenv() (err error) {
-	var ok bool
-	e.Provider, ok = os.LookupEnv("PROVIDER")
-	if !ok {
+	e.Provider = os.Getenv("PROVIDER")
+	if len(e.Provider) == 0 {
 		return errors.New("PROVIDER env not present")
 	}
-	e.VaultName, ok = os.LookupEnv("VAULT_NAME")
-	if !ok {
+	e.VaultName = os.Getenv("VAULT_NAME")
+	if len(e.VaultName) == 0 {
 		return errors.New("VAULT_NAME env not present")
 	}
-	e.ConsumerType, ok = os.LookupEnv("CONSUMER")
-	if !ok {
+	e.ConsumerType = os.Getenv("CONSUMER")
+	if len(e.ConsumerType) == 0 {
 		return errors.New("CONSUMER env var not present")
 	}
-
-	e.Namespace, ok = os.LookupEnv("SECRET_NAMESPACE")
-	if !ok {
+	e.Namespace = os.Getenv("SECRET_NAMESPACE")
+	if len(e.Namespace) == 0 {
 		e.Namespace = "default"
 	}
 	e.SecretName = os.Getenv("SECRET_NAME")
 	if len(e.SecretName) == 0 {
 		e.SecretName = e.VaultName
 	}
-
-	refreshRate, ok := os.LookupEnv("REFRESH_RATE")
-	e.RefreshRate = 60
-	if ok {
-		e.RefreshRate, err = strconv.Atoi(refreshRate)
-		if err != nil {
-			return err
-		}
-	}
-
 	deployments := os.Getenv("DEPLOYMENT_LIST")
 	if len(deployments) > 0 {
 		e.DeploymentList = strings.Split(deployments, ",")
 	}
-
 	statefulsets := os.Getenv("STATEFULSET_LIST")
 	if len(statefulsets) > 0 {
 		e.StatefulsetList = strings.Split(statefulsets, ",")
 	}
-
 	convertHyphensToUnderscores := os.Getenv("CONVERT_HYPHENS_TO_UNDERSCORES")
 	if convertHyphensToUnderscores == "true" {
 		e.ConvertHyphensToUnderscores = true
+	}
+
+	e.RefreshRate = 60
+	refreshRate := os.Getenv("REFRESH_RATE")
+	if len(refreshRate) > 0 {
+		e.RefreshRate, err = strconv.Atoi(refreshRate)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
